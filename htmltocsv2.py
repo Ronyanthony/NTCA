aa5dbb51529c94a0b24eda72452f94fddee3f458
Rony <crisrony@gmail.com>
2018-04-16 01:28:45 -0500

    no luck yet

--- htmltocsv2.py
+++ htmltocsv2.py
@@ -1,11 +1,11 @@
 #Writing a CSV from a HTML- scraping data
 
 import csv
-from urllib2 import urlopen
-from bs4 import Beautifulsoup as Bet
+import urllib
+from bs4 import Beautifulsoup
 
 html = urlopen("https://cricclubs.com/NTCA/teamSchedule.do?teamId=1636&clubId=343")
-bsobj = Bet.Beautifulsoup(html, "lxml")
+bsobj = Beautifulsoup(html, "lxml")
 table = bsobj.findall("table", {"class":"sortable table"})[0]
 rows = table.findall("tr")
 csvFile = open("ntca.csv", 'wt', newline='')
